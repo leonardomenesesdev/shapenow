de4470831b89114c6c7df7723a0db00dc30c6a68
João Victor <jvfv21212@gmail.com>
2025-05-19 14:01:30 -0300

    modifiquei a WorkoutScreen (Ela está incompleta)

--- app/src/main/java/com/example/shapenow/ui/screen/WorkoutsScreen.kt
+++ app/src/main/java/com/example/shapenow/ui/screen/WorkoutsScreen.kt
@@ -1,113 +1,84 @@
 package com.example.shapenow.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.shapenow.R
 
 @Composable
 fun WorkoutScreen(innerPadding: PaddingValues, navController: NavController) {
     var text by remember { mutableStateOf("") }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color(0xFF2F0C6D))
 
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0x88000000))
+            .padding(15.dp)
 
     ) {
+        Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFF2F0C6D), shape = RoundedCornerShape(16.dp))
+                    .padding(30.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                )
+        {
 
-        Text(
-            modifier = Modifier.padding(top = 15.dp),
-            text = "Selecione seu Treino",
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Row(modifier = Modifier.padding(top = 15.dp)) {
 
-            LeftArrowButton {}
-
-            Text(
-                modifier = Modifier.padding(10.dp),
-                text = "A",
-                textAlign = TextAlign.Center,
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-
+            Image(
+                painter = painterResource(id = R.drawable.bg_photo),
+                contentDescription = "USer",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
             )
-
-            RightArrowButton {}
-
+            Text(
+                text = "Bem vindo, user!",
+                modifier = Modifier
+                    .padding(top = 35.dp,)
+                    .padding(start = 150.dp)
+            )
 
         }
 
-        Text(
-            text = "Ombro e Peito",
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
     }
-    LazyColumn(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .padding(top = 134.dp)
-            .fillMaxSize()
-            .background(Color(0x88000000))
 
-    ) {
+    LazyColumn() {
+
+
+
 
     }
 
-}
 
-@Composable
-fun LeftArrowButton(onClick: () -> Unit) {
-    TextButton(
-        onClick = {},
-        colors = ButtonDefaults.textButtonColors(
-            contentColor = MaterialTheme.colorScheme.primary
-        )
-    ) {
-        Text("ᐊ")
 
-    }
-}
-
-@Composable
-fun RightArrowButton(onClick: () -> Unit) {
-    TextButton(
-        onClick = {}
-    ) {
-        Text("ᐅ")
-    }
 }
 
 @Preview
